0d3bf9f5d99d4a96ea736d3a50827df586e7ef16
GazinurDavletbaev <gyxtochka@mail.ru>
2026-07-15 07:12:29 +0000

    refactor clubs.py

---
+++ routers/__init__.py
@@ -0,0 +1,6 @@
+from .auth import router as auth_router
+from .games import router as games_router
+from .rating import router as rating_router
+from .protocol import router as protocol_router
+from .clubs import router as clubs_router
+from .user import router as user_router
